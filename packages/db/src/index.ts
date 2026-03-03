981c8ef3242924010eb73275a96ee18dd030448d
shubbhambajetha <110119278+shubhambajetha@users.noreply.github.com>
2026-03-04 02:56:58 +0530

    change in schema

---
+++ packages/db/src/index.ts
@@ -0,0 +1,10 @@
+export { PrismaClient } from "@prisma/client";
+export { Prisma } from "@prisma/client";
+export type {
+  User,
+  Product,
+  Order,
+  OrderItem,
+  Cart,
+  CartItem,
+} from "@prisma/client";
