0567fbf43b222671d47e0c21e757f4db90b920ee
shubbhambajetha <110119278+shubhambajetha@users.noreply.github.com>
2026-07-03 01:09:59 +0530

    change in the layout used tenstack query

--- apps/web/app/provider/ReactQueryProvider.tsx
+++ apps/web/app/provider/ReactQueryProvider.tsx
@@ -1,21 +1,20 @@
 'use client';
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 
-export default function ReactQueryProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [queryClient] = useState(() => new QueryClient());
+function makeQueryclient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 6,
+      },
+    },
+  });
+}
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-  );
+export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => makeQueryclient());
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
